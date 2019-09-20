a483903f1876f44dfd0ee39371a4a7ca46dcd06e
Karlyvall <nirvanarockk@hotmail.com>
2019-09-19 20:04:03 -0500

    Excecises for hiring

--- canvas/fizz-buzz/fizz.js
+++ canvas/fizz-buzz/fizz.js
@@ -1,1 +1,25 @@
-alert("dame empleo");
+let numeros = 100;
+let divisible = false;
+
+for (let i = 1; i <= 100; i++) {
+  if (esDivisible(i, 3)) {
+    document.write("Fizz");
+  }
+
+  if (esDivisible(i, 5)) {
+    document.write("Buzz");
+  }
+
+  if (!esDivisible(i, 3) && !esDivisible(i, 5)) {
+    document.write(i);
+  }
+  document.write("<br />");
+}
+
+function esDivisible(num, divisor) {
+  if (num % divisor == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
