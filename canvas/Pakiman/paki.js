df099c10b9c20eebe257d27ce52362c2674e4860
Karlyvall <nirvanarockk@hotmail.com>
2019-09-19 21:08:08 -0500

    Pakiman done!

--- canvas/Pakiman/paki.js
+++ canvas/Pakiman/paki.js
@@ -3,15 +3,40 @@ imagenes["Cauchin"] = "vaca.png";
 imagenes["Pokacho"] = "pollo.png";
 imagenes["Tocinauro"] = "cerdo.png";
 
-let coleccion = [];
-coleccion.push(new Pakiman("Cauchin", 100, 30));
-coleccion.push(new Pakiman("Pokacho", 80, 50));
-coleccion.push(new Pakiman("Tocinauro", 120, 40));
 
-for (let freddito of coleccion) {
-  freddito.mostrar();
+
+class Pakiman
+{
+  constructor (n, v,a){
+    this.imagen = new Image();
+    this.nombre = n;
+    this.vida = v;
+    this.ataque = a;
+    this.imagen.src = imagenes[this.nombre];
+  }
+  hablar()
+  {
+    alert(this.nombre);
+  }
+
+  mostrar()
+  {
+    document.body.appendChild(this.imagen);
+    document.write("<p>");
+    document.write("<strong>" + this.nombre + "</strong><br/>");
+    document.write("Vida: " + this.vida + "<br>");
+    document.write("Ataque: " + this.ataque + "<hr>");
+    document.write("</p>");
+  }
 }
 
-for (let x in coleccion[0]) {
-  console.log(x);
+
+let coleccion = []
+coleccion.push(new Pakiman("Cauchin", 100,30));
+coleccion.push(new Pakiman("Pokacho",80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120 ,40));
+
+for (let paki of coleccion)
+{
+    paki.mostrar();
 }
